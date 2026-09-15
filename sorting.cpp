ec4db2550268f787a041a3fd28ecad3d49ba240e
Plaksha <learnwithplaksha@gmail.com>
2026-09-16 00:02:42 +0530

    Found second largest number using another loop with intmin and used 2 elements to solve it

--- sorting.cpp
+++ sorting.cpp
@@ -6,14 +6,20 @@ int larg(vector <int> &x) {
 }
 int larg2(vector <int> &a) {
     int maxi = INT_MIN;
+    int y = INT_MIN;
     for (auto i: a) {
         if ( maxi < i) {
             maxi = i;
         }
     }
-    return maxi;
+    for (auto j:a) {
+        if ( y < j && j< maxi) {
+            y = j;
+        }
+    }
+    return y;
 }
 int main () {
-    vector<int> v = {-2,1,3,4,-3,4,2,6,5,3,7,5,2,1};
+    vector<int> v = {-2,1,3,4,-3,4,2,5,3,7,6,5,2,1};
     cout<<larg2(v)<<endl;
 }
