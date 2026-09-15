46fab4aa6ce443c6e0e0958c3946b19b8bcce1b1
Plaksha <learnwithplaksha@gmail.com>
2026-09-15 23:39:40 +0530

    Found largest element in vector using intmin , changing value

--- sorting.cpp
+++ sorting.cpp
@@ -4,7 +4,16 @@ int larg(vector <int> &x) {
     sort(x.begin(), x.end());
     return x[size(x)-1];
 }
+int larg2(vector <int> &a) {
+    int maxi = INT_MIN;
+    for (auto i: a) {
+        if ( maxi < i) {
+            maxi = i;
+        }
+    }
+    return maxi;
+}
 int main () {
-    vector<int> v = {-2,1,3,4,-3,4,2};
-    cout<<larg(v)<<endl;
+    vector<int> v = {-2,1,3,4,-3,4,2,6,5,3,7,5,2,1};
+    cout<<larg2(v)<<endl;
 }
