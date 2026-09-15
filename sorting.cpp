419d2339e5a6ea19894fdcf1a1ced201d4860f9d
Plaksha <learnwithplaksha@gmail.com>
2026-09-15 23:29:45 +0530

    Found largest element in vector using sort directly

--- sorting.cpp
+++ sorting.cpp
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+int larg(vector <int> &x) {
+    sort(x.begin(), x.end());
+    return x[size(x)-1];
+}
 int main () {
-    cout<<"Hello World!"<<endl;
+    vector<int> v = {-2,1,3,4,-3,4,2};
+    cout<<larg(v)<<endl;
 }
