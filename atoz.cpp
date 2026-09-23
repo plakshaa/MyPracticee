b6b4862a95e950b9c942d93633806f2444640c0c
Plaksha <learnwithplaksha@gmail.com>
2026-09-23 23:56:03 +0530

    Array rotate to left by k times

--- atoz.cpp
+++ atoz.cpp
@@ -1,5 +1,16 @@
+// class Solution {
+// public:
+//     void rotateArray(vector<int>& nums, int k) {
+//         for (int a =0; a<k;a++){
+//             int n = nums[0];
+//             int s = nums.size();
+//             for (int i = 0; i<= s - 1; i++){
+//                 nums[i] = nums[i+1];
+//                 if(i == s-1){
+//                     nums[i] = n;
+//                 }
+//             }
+//             cout<<&nums<<endl;
+//         }}
 //
-// Created by LOQ on 23-09-2026.
-//
-
-#include "atoz.h"
+// };
