542ba66a8c061e8c5d6636aa8acad34bb21736ea
Plaksha <learnwithplaksha@gmail.com>
2026-09-27 00:48:47 +0530

    Used separate loops to rotate the array to right at once, without doing it each time.

--- atoz.cpp
+++ atoz.cpp
@@ -14,3 +14,33 @@
 //         }}
 //
 // };
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(k > nums.size()){
+            k = k % nums.size();
+        }
+        vector<int> num;
+        int s = nums.size();
+        for(int i = s - k; i<= s-1; i++){
+            num.push_back(nums[i]);
+        }
+        for( int j = 0; j<= s - k -1; j++){
+            num.push_back(nums[j]);
+        }
+        nums = num;
+        //     for (int a =0; a<k;a++){
+        //     int s = nums.size();
+        //     int n = nums[s-1];
+        //     for (int i = s-1; i>0; i--){
+        //         nums[i] = nums[i-1];
+        //         if(i == 1){
+        //             nums[0] = n;
+        //         }
+        //     }
+        //     cout<<&nums<<endl;
+        // }
+        // }
+    }
+};
